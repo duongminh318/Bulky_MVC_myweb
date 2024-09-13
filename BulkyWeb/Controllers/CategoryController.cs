0f6a729f4818b3ebe088ec96f2e66b2505c78bb1
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-09-13 16:08:47 +0700

    43. Validation Summary

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -26,10 +26,10 @@ namespace BulkyWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
-            if (obj.Name == obj.DisplayOrder.ToString())
+           /* if (obj.Name == obj.DisplayOrder.ToString())
             {
                 ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
-            }
+            }*/
             if (ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
