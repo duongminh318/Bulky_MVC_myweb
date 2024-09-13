d66337d1f09ff00f7d6995562345a19b31d36834
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-09-13 15:35:26 +0700

    40. Create Category (controller post)

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -17,9 +17,18 @@ namespace BulkyWeb.Controllers
             return View(objCategoryList);
         }
 
+        // add
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
