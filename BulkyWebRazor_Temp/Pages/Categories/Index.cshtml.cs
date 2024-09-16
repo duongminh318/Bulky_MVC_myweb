0d397e2bc137f76929f3d83c31a14f495ea42227
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-09-16 17:07:16 +0700

    58. Display all Categories

--- BulkyWebRazor_Temp/Pages/Categories/Index.cshtml.cs
+++ BulkyWebRazor_Temp/Pages/Categories/Index.cshtml.cs
@@ -1,12 +1,22 @@
+﻿using BulkyWebRazor_Temp.Data;
+using BulkyWebRazor_Temp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BulkyWebRazor_Temp.Pages.Categories
 {
+    // file này giông như controller vậy
     public class IndexModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
+        public List<Category> CategoryList { get; set; }
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public void OnGet()
         {
+            CategoryList = _db.Categories.ToList();
         }
     }
 }
