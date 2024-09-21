770c533ab71cb9293c8de7aa87e62d2d499b7ba4
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-09-21 14:08:43 +0700

    77. Unit Of Work Implementation (tạo ra 1 đơn vị công việc UnitOfWork chứa các hàm global)

--- Bulky.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ Bulky.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -1,5 +1,4 @@
-﻿using Bulky.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace Bulky.DataAccess.Repository.IRepository
 {
-    public interface ICategoryRepository:IRepository<Category>
+    public interface IUnitOfWork
     {
-        void Update(Category obj);
-       // void Save();
+        ICategoryRepository Category { get; }
+        void Save();
     }
 }
