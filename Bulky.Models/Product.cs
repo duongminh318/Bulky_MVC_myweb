3d3d1267a77e7f3ed99628e7323977915af0ead6
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-09-26 17:58:53 +0700

    87. Add Image Url Column (thêm file hình ảnh vào product table)

--- Bulky.Models/Product.cs
+++ Bulky.Models/Product.cs
@@ -43,5 +43,6 @@ namespace Bulky.Models
         public int CategoryId { get; set; }
         [ForeignKey("CategoryId")]
         public Category Category { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
