f0df28f0d986cf10b9a0e576b36b1262cf6adb96
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-09-26 17:46:03 +0700

    86. Add Foreign Key in EF Core (CategoryId) (thêm khoá ngoại CategoryId cho bảng Product)

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -7,10 +7,12 @@ namespace Bulky.DataAccess.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-            
-         }
+
+        }
+
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Category>().HasData(
@@ -19,7 +21,6 @@ namespace Bulky.DataAccess.Data
                 new Category { Id = 3, Name = "History", DisplayOrder = 3 }
                 );
 
-            // product
             modelBuilder.Entity<Product>().HasData(
                 new Product
                 {
@@ -31,7 +32,8 @@ namespace Bulky.DataAccess.Data
                     ListPrice = 99,
                     Price = 90,
                     Price50 = 85,
-                    Price100 = 80
+                    Price100 = 80,
+                    CategoryId = 1
                 },
                 new Product
                 {
@@ -43,7 +45,8 @@ namespace Bulky.DataAccess.Data
                     ListPrice = 40,
                     Price = 30,
                     Price50 = 25,
-                    Price100 = 20
+                    Price100 = 20,
+                    CategoryId = 1
                 },
                 new Product
                 {
@@ -55,7 +58,8 @@ namespace Bulky.DataAccess.Data
                     ListPrice = 55,
                     Price = 50,
                     Price50 = 40,
-                    Price100 = 35
+                    Price100 = 35,
+                    CategoryId = 1
                 },
                 new Product
                 {
@@ -67,7 +71,8 @@ namespace Bulky.DataAccess.Data
                     ListPrice = 70,
                     Price = 65,
                     Price50 = 60,
-                    Price100 = 55
+                    Price100 = 55,
+                    CategoryId = 2
                 },
                 new Product
                 {
@@ -79,7 +84,8 @@ namespace Bulky.DataAccess.Data
                     ListPrice = 30,
                     Price = 27,
                     Price50 = 25,
-                    Price100 = 20
+                    Price100 = 20,
+                    CategoryId = 2
                 },
                 new Product
                 {
@@ -91,7 +97,8 @@ namespace Bulky.DataAccess.Data
                     ListPrice = 25,
                     Price = 23,
                     Price50 = 22,
-                    Price100 = 20
+                    Price100 = 20,
+                    CategoryId = 3
                 }
                 );
         }
