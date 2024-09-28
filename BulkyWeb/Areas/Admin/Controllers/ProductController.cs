8d448b5cd8561b4d0e0d4bc6bf2cf3c3c29211e1
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-09-28 14:10:50 +0700

    90. ViewData in Action (công dụng giống như viewbag nhưng Dữ liệu lưu trữ dạng key-value, cần phải cast dữ liệu khi sử dụng)

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -31,7 +31,8 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
                Text = u.Name,
                Value = u.Id.ToString()
            });
-            ViewBag.CategoryList = CategoryList;
+            //ViewBag.CategoryList = CategoryList;
+            ViewData["CategoryList"] = CategoryList;
             return View();
         }
         [HttpPost]
