0f12a4a84099bf6cdcdae18e057597789257e01b
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-10-01 11:32:42 +0700

    100. Loading Navigation Properties (hiển thị category cho mỗi product)

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -19,7 +19,8 @@ namespace BulkyWeb.Areas.Admin.Controllers
         }
         public IActionResult Index()
         {
-            List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();
+            /* List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();*/
+            List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
 
             return View(objProductList);
         }
