012abde32a3622f3a6d651505a6f69871c493794
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-10-04 17:44:45 +0700

    115. Extend Identity User (mở rộng người dùng, thêm một số có AspNetUsers có sẵn )

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -14,6 +14,7 @@ namespace Bulky.DataAccess.Data
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
