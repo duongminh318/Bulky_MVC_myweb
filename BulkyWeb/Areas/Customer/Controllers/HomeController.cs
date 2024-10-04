77cec06e09b8fadc5a4d3693126f57e08d641540
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-10-04 11:40:59 +0700

    108. Details Action Method (bắt link cho button detail product)

--- BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -24,6 +24,12 @@ namespace BulkyWeb.Areas.Customer.Controllers
             return View(productList);
         }
 
+        public IActionResult Details(int productId)
+        {
+            Product product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category");
+            return View(product);
+        }
+
         public IActionResult Privacy()
         {
             return View();
