fffc490b7ef2515423d1afdcd12d85078aef0ff2
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-10-08 15:59:24 +0700

    126.3. Assignment 3 (View) (hiển thị company lên view, tương tự như với product)

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -12,7 +12,7 @@ namespace BulkyWeb.Areas.Admin.Controllers
 {
 
     [Area("Admin")]
-    [Authorize(Roles = SD.Role_Admin)]
+    //[Authorize(Roles = SD.Role_Admin)]
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
