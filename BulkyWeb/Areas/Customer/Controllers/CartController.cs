ea9c973245f657694d2998373391956245e9c9ca
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-10-10 20:02:34 +0700

    137. Shopping Cart UI (load giao diện trang giỏ hàng)

---
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Areas.Customer.Controllers
+{
+    [Area("customer")]
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
