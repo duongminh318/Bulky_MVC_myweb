7848f219e11299a619d538d05a1077b59832f49d
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-10-10 20:30:28 +0700

    138. Get Shopping Cart (xử lý logic lấy ra giỏ hàng)

--- Bulky.DataAccess/Repository/Repository.cs
+++ Bulky.DataAccess/Repository/Repository.cs
@@ -54,9 +54,13 @@ namespace Bulky.DataAccess.Repository
 
         }
 
-        public IEnumerable<T> GetAll(string? includeProperties = null)
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter, string? includeProperties = null)
         {
             IQueryable<T> query = dbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
             if (!string.IsNullOrEmpty(includeProperties))
             {
                 foreach (var includeProp in includeProperties
--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -1,13 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace BulkyWeb.Areas.Customer.Controllers
 {
     [Area("customer")]
+    [Authorize]
     public class CartController : Controller
     {
+
+        private readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartVM ShoppingCartVM { get; set; }
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+
         public IActionResult Index()
         {
-            return View();
+
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            ShoppingCartVM = new()
+            {
+                ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId,
+                includeProperties: "Product")
+            };
+
+            return View(ShoppingCartVM);
         }
     }
 }
