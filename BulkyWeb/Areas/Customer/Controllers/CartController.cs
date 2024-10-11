f04921b02a667a9c4cbc0e07be223343c51c9c26
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-10-11 11:16:27 +0700

    139.2. Get order total in Shopping Cart (edit CartController để tính tổng giá trong giỏ hàng)

--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -35,8 +35,8 @@ namespace BulkyWeb.Areas.Customer.Controllers
 
             foreach (var cart in ShoppingCartVM.ShoppingCartList)
             {
-                double price = GetPriceBasedOnQuantity(cart);
-                ShoppingCartVM.OrderTotal += (price * cart.Count);
+                cart.Price = GetPriceBasedOnQuantity(cart);
+                ShoppingCartVM.OrderTotal += (cart.Price * cart.Count);
             }
 
             return View(ShoppingCartVM);
