52b6ac0d8311242a233be4676186091b8e4141c2
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-10-12 16:25:03 +0700

    143. Create Order Header and Details Model (add Order Hearder and Order Detail tables to DB)

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -17,7 +17,8 @@ namespace Bulky.DataAccess.Data
         public DbSet<Company> Companies { get; set; }
         public DbSet<ShoppingCart> ShoppingCarts { get; set; }
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
-
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
