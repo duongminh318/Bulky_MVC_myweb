4e6aa68e7c4d4a9ea52956b1661df3275745db40
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-10-12 17:02:11 +0700

    145. Make ShoppingCartVM more Dynamic (thêm 1 số thuộc tình làm cho shoppingcartvm linh hoạt hơn)

--- Bulky.Models/ViewModels/ShoppingCartVM.cs
+++ Bulky.Models/ViewModels/ShoppingCartVM.cs
@@ -10,6 +10,6 @@ namespace Bulky.Models.ViewModels
     {
         public IEnumerable<ShoppingCart> ShoppingCartList { get; set; }
 
-        public double OrderTotal { get; set; }
+        public OrderHeader OrderHeader { get; set; }
     }
 }
