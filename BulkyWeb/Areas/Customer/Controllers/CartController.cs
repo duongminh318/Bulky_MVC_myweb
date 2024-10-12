45c8424ef6c62ee5751bf6ddea58596fcdd4c6a3
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-10-12 16:01:01 +0700

    142. Order Summary UI (Total payment sheet interface)

--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -42,6 +42,11 @@ namespace BulkyWeb.Areas.Customer.Controllers
             return View(ShoppingCartVM);
         }
 
+        public IActionResult Summary()
+        {
+            return View();
+        }
+
 
         public IActionResult Plus(int cartId)
         {
