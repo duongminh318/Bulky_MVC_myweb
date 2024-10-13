3d7ba4d6c6e7f2b56d4467e556d0cd289401d1cd
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-10-13 17:05:53 +0700

    154. Add Helper Methods in Order Header Repository (Thêm các phương thức trợ giúp trong kho lưu trữ tiêu đề đơn hàng, SessionId thanh toán)

--- Bulky.Models/OrderHeader.cs
+++ Bulky.Models/OrderHeader.cs
@@ -29,6 +29,7 @@ namespace Bulky.Models
         public DateTime PaymentDate { get; set; }
         public DateOnly PaymentDueDate { get; set; }
 
+        public string? SessionId { get; set; }
         public string? PaymentIntentId { get; set; }
 
         [Required]
