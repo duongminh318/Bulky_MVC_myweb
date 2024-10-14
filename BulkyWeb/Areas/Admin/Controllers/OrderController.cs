a1c6277d64443f8e1758a2596e7f669efbee8215
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-10-14 09:30:43 +0700

    158. OrderVM and Order Controller (add ordervm and OrderController, load đơn hàng)

---
+++ BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,28 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Areas.Admin.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            return Json(new { data = objOrderHeaders });
+        }
+        #endregion
+    }
+
+}
