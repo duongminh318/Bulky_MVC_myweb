9d9166be2c7c13dea926f922d2c2c7785654d14c
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-10-16 23:57:55 +0700

    175. Bug Solution and Logout (sửa bug khi xoá sp trong giỏ hảng, logout mất đi session giỏ hàng)

--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -203,8 +203,8 @@ namespace BulkyWeb.Areas.Customer.Controllers
 
 		public IActionResult Minus(int cartId)
 		{
-			var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
-			if (cartFromDb.Count <= 1)
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId, tracked: true);
+            if (cartFromDb.Count <= 1)
 			{
                 //remove that from cart
                 HttpContext.Session.SetInt32(SD.SessionCart, _unitOfWork.ShoppingCart
@@ -223,7 +223,7 @@ namespace BulkyWeb.Areas.Customer.Controllers
 
 		public IActionResult Remove(int cartId)
 		{
-			var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId, tracked: true);
             HttpContext.Session.SetInt32(SD.SessionCart, _unitOfWork.ShoppingCart
              .GetAll(u => u.ApplicationUserId == cartFromDb.ApplicationUserId).Count() - 1);
             _unitOfWork.ShoppingCart.Remove(cartFromDb);
