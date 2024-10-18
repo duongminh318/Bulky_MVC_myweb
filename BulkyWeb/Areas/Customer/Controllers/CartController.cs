c1c0f86d03eb84022f06345dd2ac738a4b810c3e
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-10-18 11:46:53 +0700

    181. Session Bug (xoá giỏ hàng khi thanh toán thành công)

--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -181,13 +181,13 @@ namespace BulkyWeb.Areas.Customer.Controllers
                     _unitOfWork.OrderHeader.UpdateStripePaymentID(id, session.Id, session.PaymentIntentId);
                     _unitOfWork.OrderHeader.UpdateStatus(id, SD.StatusApproved, SD.PaymentStatusApproved);
                     _unitOfWork.Save();
+                    HttpContext.Session.Clear();
                 }
             }
             List<ShoppingCart> shoppingCarts = _unitOfWork.ShoppingCart
                 .GetAll(u => u.ApplicationUserId == orderHeader.ApplicationUserId).ToList();
             _unitOfWork.ShoppingCart.RemoveRange(shoppingCarts);
             _unitOfWork.Save();
-
             return View(id);
 		}
 
