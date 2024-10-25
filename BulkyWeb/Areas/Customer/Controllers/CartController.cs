c0e8d009a35ca35e307fc1ccf2f26ca4839a0af2
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-10-25 12:05:52 +0700

    190. Azure Deployment in Action ( Post Deployment Url Settings )

--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -130,7 +130,8 @@ namespace BulkyWeb.Areas.Customer.Controllers
 			{
                 //it is a regular customer account and we need to capture payment
                 //stripe logic
-                var domain = "https://localhost:7054/";
+                //var domain = "https://localhost:7054/";
+                var domain = Request.Scheme + "://" + Request.Host.Value + "/";
                 var options = new SessionCreateOptions
                 {
                     SuccessUrl = domain + $"customer/cart/OrderConfirmation?id={ShoppingCartVM.OrderHeader.Id}",
