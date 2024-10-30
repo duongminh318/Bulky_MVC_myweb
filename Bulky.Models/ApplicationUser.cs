aa693e1338dbf04f488105d2a6b0d1b8a609b6cf
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-10-30 17:39:00 +0700

    194. Display Company Name

--- Bulky.Models/ApplicationUser.cs
+++ Bulky.Models/ApplicationUser.cs
@@ -22,6 +22,6 @@ namespace Bulky.Models
         public int? CompanyId { get; set; }
         [ForeignKey("CompanyId")]
         [ValidateNever]
-        public Company Company { get; set; }
+        public Company? Company { get; set; }
     }
 }
