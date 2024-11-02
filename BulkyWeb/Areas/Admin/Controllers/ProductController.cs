1fba6cb3dfb1b61cb4d3c11c13d2a805408ca587
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-11-02 16:36:24 +0700

    209. Delete Product (delete both folder img of this product)

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -165,15 +165,17 @@ namespace BulkyWeb.Areas.Admin.Controllers
             {
                 return Json(new { success = false, message = "Error while deleting" });
             }
-
-            //var oldImagePath =
-            //               Path.Combine(_webHostEnvironment.WebRootPath, 
-            //               productToBeDeleted.ImageUrl.TrimStart('\\'));
-
-            //if (System.IO.File.Exists(oldImagePath))
-            //{
-            //    System.IO.File.Delete(oldImagePath);
-            //}
+            string productPath = @"images\products\product-" + id;
+            string finalPath = Path.Combine(_webHostEnvironment.WebRootPath, productPath);
+            if (Directory.Exists(finalPath))
+            {
+                string[] filePaths = Directory.GetFiles(finalPath);
+                foreach (string filePath in filePaths)
+                {
+                    System.IO.File.Delete(filePath);
+                }
+                Directory.Delete(finalPath);
+            }
 
             _unitOfWork.Product.Remove(productToBeDeleted);
             _unitOfWork.Save();
